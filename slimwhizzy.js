421da905082b0b22ec91e53114cd0b8c7fb857ca
Jody <jody@jodylecompte.com>
2016-09-27 01:47:28 -0500

    Further work on demo template / beginning of functionality

--- slimwhizzy.js
+++ slimwhizzy.js
@@ -6,5 +6,12 @@
 // *******************************
 var buttons = document.getElementsByClassName("toolbar-button");
 for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', processCommand, false);
+    var command = buttons[i].getAttribute('data-command');
+    buttons[i].addEventListener('click', function() {
+        processCommand(command);
+    }, false);
+}
+
+function processCommand(command) {
+    document.execCommand(command, false, null)
 }
