36c4735967ae4d0a2d29d11e8eef09a74675fbae
Jody LeCompte <jlecompte@proexel.com>
2016-09-27 21:42:35 -0500

    Rough layout of the structure to handle commands and utilize document.execCommand successfully and some lazy copy and paste prototyping of the demo

--- slimwhizzy.js
+++ slimwhizzy.js
@@ -8,13 +8,27 @@ var buttons = document.getElementsByClassName("toolbar-button");
 
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function() {
-        var command = this.getAttribute('data-command');
-        processCommand(command);
+        processCommand(this.getAttribute('data-command'));
     }, false);
 
 }
 
 function processCommand(command) {
-    console.log(command);
-    document.execCommand(command, false, null);
+    var directCommands = [
+        'bold', 'italic', 'underline'
+    ];
+
+    var blockCommands = [
+        'h1', 'h2', 'h3', 'h4'
+    ];
+
+    if(directCommands.indexOf(command) !== -1) {
+        document.execCommand(command, false, null);
+    }
+
+    if(blockCommands.indexOf(command) !== -1) {
+        document.execCommand(formatBlock, false, command);
+    }
+
+    //document.execCommand(command, false, null);
 }
