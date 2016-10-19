6e2c8df073730399cdd50a7531c4fc582f1b9a43
Jody LeCompte <jody@jodylecompte.com>
2016-10-18 20:35:37 -0500

    Removed merge conflict junk

--- slimwhizzy.js
+++ slimwhizzy.js
@@ -5,14 +5,11 @@
 // * License: MIT License
 // *******************************
 var buttons = document.getElementsByClassName("toolbar-button");
-<<<<<<< HEAD
-if(window.location.hash){
-    document.querySelectorAll('div[contenteditable]')[0].innerHTML = window.atob(window.location.hash.slice(1))
-=======
+
 if(window.location.search){
     document.querySelectorAll('div[contenteditable]')[0].innerHTML = window.atob(window.location.search.slice(1))
->>>>>>> 0470c2c02add456131f82855e234b186ba2539e9
 }
+
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function() {
         processCommand(this.getAttribute('data-command'));
@@ -34,11 +31,7 @@ function processCommand(command) {
         var listArray = ['item1', 'item 2'];
         document.execCommand(command);
     } else if(command == "save"){
-<<<<<<< HEAD
-        window.location.hash = window.btoa(document.querySelectorAll('div[contenteditable]')[0].innerHTML)
-=======
         window.location.search = window.btoa(document.querySelectorAll('div[contenteditable]')[0].innerHTML);
->>>>>>> 0470c2c02add456131f82855e234b186ba2539e9
     }else{
         document.execCommand(command, false, null);
     }
